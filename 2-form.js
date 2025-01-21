f4750bcffa5f44de428086d9e15720d05ca7aabd
Yevhen550 <111044917+Yevhen550@users.noreply.github.com>
2025-01-21 14:36:44 +0000

    Deploying to gh-pages from @ Yevhen550/goit-advancedjs-hw-01@e586c50b388fbc6fc8f35247265a05c63e4511ff 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const e=document.querySelector(".feedback-form");let t={};e.addEventListener("input",a);function a(o){t={email:o.target.value},console.log("🚀 => onFormInput => localStorData:",t),localStorage.setItem("email",JSON.stringify(t))}
+//# sourceMappingURL=2-form.js.map
