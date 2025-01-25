a8379b2c84d30a7965e0538e69a481ec51715044
Yevhen <benatanlev@icloud.com>
2025-01-25 13:38:56 +0200

    task-2  done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,16 +1,55 @@
 const formEl = document.querySelector('.feedback-form');
 
-let localStorData = {};
+let formData = {
+  email: '',
+  message: '',
+};
 
-formEl.addEventListener('input', onFormInput);
+function loadSavedData() {
+  try {
+    const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    if (savedData === null) {
+      return;
+    }
+
+    const savedDataKeys = Object.keys(savedData);
+
+    savedDataKeys.forEach(key => {
+      formEl.elements[key].value = savedData[key];
+      formData[key] = savedData[key];
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+loadSavedData();
 
 function onFormInput(event) {
-  const valueInput = event.target.value;
+  const formField = event.target;
+  const fieldName = formField.name;
+  const fieldValue = formField.value.trim();
 
-  localStorData = {
-    email: valueInput,
-  };
-  console.log('🚀 => onFormInput => localStorData:', localStorData);
+  formData[fieldName] = fieldValue;
 
-  localStorage.setItem('email', JSON.stringify(localStorData));
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+  formData = { email: '', message: '' };
+
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+formEl.addEventListener('input', onFormInput);
+formEl.addEventListener('submit', onFormSubmit);
